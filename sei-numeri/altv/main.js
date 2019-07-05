e4f67f6914ef8621768309fd210c91ab2429049f
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-05 18:24:40 +0200

    verifico caratteri scritti nel prompt

--- sei-numeri/altv/main.js
+++ sei-numeri/altv/main.js
@@ -3,26 +3,40 @@
 // se dispari pushalo nell-array
 
 var array = [];
+var valore = "";
+
 for (var i = 1; i <= 6; i++) {
-
-  var ask = parseInt(prompt('numero' + i));
-  // // ask trasforma in numero la stringa cosasei
-  // var ask = parseInt(cosaSei);
-  // if (cosaSei == "") {
-  //   console.log('non ha scritto il numero' + i);
-  //   i--;
-  // }
-  // console.log(typeof ask);
-  // // if (ask.typeOf != string) {
-  // //   console.log('non va bene');;
-  // // }
-  if (ask % 2 !=0) {
-    array.push(ask);
-    console.log(array);
-  }
-
+//chiedo valore ad utente
+var inserito = prompt('inserisci un valore',valore);
+// se il valore inserito = empty; come era gia all'inizio (vedi var valore)
+// allora log scrive messaggio qui sotto
+if (inserito === "") {
+    console.log('non hai scritto niente');
 }
-
+//se il valore inserito non è un numero quindi isNaN
+//allora scrivi messaggio qui sotto
+ else if (isNaN(inserito)){
+  console.log(inserito);
+  console.log('isNan non so che fare');
+}
+//se inserito esiste ed è a questo punto verificato che è un numero
+//se il resto di inserito / 2 != 0 inseriscilo nell-array;
+//il resto di inserito / 2 == 0 scrivi che è pari.
+else if (inserito) {
+    if (inserito % 2 !=0) {
+      array.push(inserito);
+      console.log(array);
+    }
+    else if (inserito % 2 ==0){
+      console.log(inserito + ' è pari');
+    }
+    else {
+      console.log('');
+    }
+} else {
+    console.log('come cosa hai scritto?!!!!??');
+}
+}
 
 
 //
